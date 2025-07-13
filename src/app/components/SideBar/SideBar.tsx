585312ef163b86da0d56cc0cf23dfb635838849a
Simon Kovtyk <simon@kovtyk.com>
2025-07-13 23:37:53 +0200

    feat: create design (#5)
    Co-authored-by: David Ax <davidax200@gmail.com>

---
+++ src/app/components/SideBar/SideBar.tsx
@@ -0,0 +1,16 @@
+import {FC} from 'react';
+
+
+interface IProps {
+}
+
+const SideBar: FC<IProps> = (props) => {
+    return (
+        <div>
+            <h1>SideBar</h1>
+        </div>
+    );
+};
+
+
+export default SideBar;
